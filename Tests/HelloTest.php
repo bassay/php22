1b5bdfe89474ddb475b29bb78e82d1d9433a2326
bassa <test@test.ru>
2022-11-18 19:36:25 +0300

    update

---
+++ Tests/HelloTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bassa\Php2\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class HelloTest extends TestCase {
+
+  public function testItWorks(): void {
+    // Проверяем, что true – это true
+    $this->assertTrue(TRUE);
+  }
+
+}
