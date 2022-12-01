10bdc96c4a1cbd6a6e58fe2e9875dfb785017cfa
bassa <test@test.ru>
2022-12-01 15:16:04 +0300

    dz5 update

--- index.php
+++ index.php
@@ -6,24 +6,24 @@
 //}
 //echo One::class;
 
-//echo "<h1>Привет мир!</h1>";
+echo "<h1>Привет мир!</h1>";
 
 // Функция с двумя параметрами возвращает строку
 
 
-function someFunction(bool $one, int $two = 123,): string
-{
-  return $one . $two;
-}
-// Создаём объект рефлексии
-// Передаём ему имя интересующей нас функции
-$reflection = new ReflectionFunction('someFunction');
-// Получаем тип возвращаемого функцией значения
-echo $reflection->getReturnType()->getName() . "\n <br>";
-//echo var_dump($reflection->getReturnType()->getName()) . "\n <br>";
-// Получаем параметры функции
-foreach ($reflection->getParameters() as $parameter) {
-  // Для каждого параметра функции
-  // получаем его имя и тип
-  echo $parameter->getName().'-> ['.$parameter->getType()->getName()."]\n <br>";
-}
+//function someFunction(bool $one, int $two = 123,): string
+//{
+//  return $one . $two;
+//}
+//// Создаём объект рефлексии
+//// Передаём ему имя интересующей нас функции
+//$reflection = new ReflectionFunction('someFunction');
+//// Получаем тип возвращаемого функцией значения
+//echo $reflection->getReturnType()->getName() . "\n <br>";
+////echo var_dump($reflection->getReturnType()->getName()) . "\n <br>";
+//// Получаем параметры функции
+//foreach ($reflection->getParameters() as $parameter) {
+//  // Для каждого параметра функции
+//  // получаем его имя и тип
+//  echo $parameter->getName().'-> ['.$parameter->getType()->getName()."]\n <br>";
+//}
